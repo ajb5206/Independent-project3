800a562595f611f75a1adcfa338257e412bc6c6f
Alex Bertotto <ajb5206@gmail.com>
2021-07-05 17:51:32 -0700

    perfom catch-all commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,40 @@
 
 let subArray;
 let numArray;
-let newString;
-let userNumber
-function arrayMaker() {
+let userString;
+let userNumber;
+
+function arrayMaker(_number) {
   for (let i = 0; i <= userNumber.length; i++) {
 	  subArray.push(i);
 	  numArray.push(subArray.concat());
 		return numArray.toString();
   }; 
-}
+};
 
-function regexReplacer() {
-	newString.replace(/\d{1,9}*3\d{1,9}*/g, "Won't you be my neighbor?");
-	newString.replace(/\d{1,9}*2\d{1,9}*/g, "Boop!");
-	newString.replace(/\d{1,9}*1\d{1,9}*/g, "Beep!");
-  return newString;
-}
+function regexReplacer(_string) {
+	userString.replace(/\d{1,9}*3\d{1,9}*/g, "Won't you be my neighbor?");
+	userString.replace(/\d{1,9}*2\d{1,9}*/g, "Boop!");
+	userString.replace(/\d{1,9}*1\d{1,9}*/g, "Beep!");
+  return userString;
+};
 
 //user logic
 
 $(document).ready(function() {
 	$("form#inputForm").submit(function(event) {
 		event.preventDefault();
-			userNumber = parseInt($("#input1").val());
-			newString = arrayMaker(userNumber);
+			const userNumber = parseInt($("#input1").val());
+			userString = arrayMaker(userNumber);
   });
+	return userString;
 });
 
 
 $(document).ready(function() {
 	$("form#inputForm").submit(function(event) {
 		event.preventDefault();
-		const output = regexReplacer(newString);
+		const output = regexReplacer(userString);
 		$("#output").text(output);
 	});
 });
