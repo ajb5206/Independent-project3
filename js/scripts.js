76ca1b0f685afce9f1b97b0b1f9612e44afef396
Alex Bertotto <ajb5206@gmail.com>
2021-07-10 11:31:29 -0700

    remove text from js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,32 @@
 //business logic
 
-let subArray;
-let numArray;
-let userString;
-let userNumber;
 
-function arrayMaker(_number) {
-  for (let i = 0; i <= userNumber.length; i++) {
-	  subArray.push(i);
-	  numArray.push(subArray.concat());
-		return numArray.toString();
+function arrayMaker(number) {
+  let numArray = [];
+	let
+	for (let i = 0; i <= userInput; i++) {
+		if i.toString includes("3") i.push "won't you be my neightbor".
+		else if i.toString.includes(2) i.push boop
+		else if i.toString.incudes(1) i.push beep
+		else 
   }; 
+	return 
 };
 
-function regexReplacer(_string) {
-	userString.replace(/\d{1,9}*3\d{1,9}*/g, "Won't you be my neighbor?");
-	userString.replace(/\d{1,9}*2\d{1,9}*/g, "Boop!");
-	userString.replace(/\d{1,9}*1\d{1,9}*/g, "Beep!");
-  return userString;
-};
 
 //user logic
 
 $(document).ready(function() {
 	$("form#inputForm").submit(function(event) {
 		event.preventDefault();
-			const userNumber = parseInt($("#input1").val());
-			userString = arrayMaker(userNumber);
+		const userInput = parseInt($("#input1").val());
+		const userString = arrayMaker(userInput);
+		// const output = regexReplacer(userString);
+		$("#output").text(userString);
   });
 	return userString;
 });
 
 
-$(document).ready(function() {
-	$("form#inputForm").submit(function(event) {
-		event.preventDefault();
-		const output = regexReplacer(userString);
-		$("#output").text(output);
-	});
-});
 
 
