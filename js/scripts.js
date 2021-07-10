b38512d1b89ed317be5011a0f4508a58a9e51725
Alex Bertotto <ajb5206@gmail.com>
2021-07-10 12:06:43 -0700

    testing for 0 returns 0

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,13 @@ function arrayMaker(userInput) {
   let numArray = [];
 	for (let i = 0; i <= userInput; i++) {
 		const outputString = i.toString();
-		if (outputString.includes(3)) {
-			numArray.push("Won't you be my neightbor?")
-		}
-		else if (outputString.includes(2)) {
-			numArray.push("Boop!")
-		} 
-		else if (outputString.includes(1)) {
+		//if (outputString.includes(3)) {
+		//	numArray.push("Won't you be my neightbor?")
+		//}
+		//else if (outputString.includes(2)) {
+		//	numArray.push("Boop!")
+		//} 
+		if (outputString.includes(1)) {
 			numArray.push("Beep!")
 		} 
 		else {
